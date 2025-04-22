2eb45241257bf1ba40dd7e627adb0757b22b1bda
palme <palme@LEGION>
2025-04-22 14:37:36 -0500

    Add project files.

---
+++ FormMain.cs
@@ -0,0 +1,10 @@
+namespace VideoGameCollection_WinForms
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
