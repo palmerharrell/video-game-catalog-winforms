f064075438e987230835532498c79492f81d1239
Palmer Harrell <palmerharrell@msn.com>
2025-04-22 16:23:23 -0500

    Initial Sql setup and testing

--- MainDebugger.cs
+++ MainDebugger.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VideoGameCollection_WinForms.Repositories;
 
 namespace VideoGameCollection_WinForms
 {
@@ -12,6 +14,42 @@ namespace VideoGameCollection_WinForms
         public static void MainDebug()
         {
             // Test Code goes here
+
+            // TODO: Research, setup, and test connection to network SQL Server database 
+
+            GamesSqlRepo.GetGames();
+
+            //try
+
+            //{
+
+            //    String str = "server=MUNESH-PC;database=windowapp;UID=sa;password=123";
+
+            //    String query = "select * from data";
+
+            //    SqlConnection con = new SqlConnection(str);
+
+            //    SqlCommand cmd = new SqlCommand(query, con);
+
+            //    con.Open();
+
+            //    DataSet ds = new DataSet();
+
+            //    MessageBox.Show("connect with sql server");
+
+            //    con.Close();
+
+            //}
+
+            //catch (Exception es)
+
+            //{
+
+            //    MessageBox.Show(es.Message);
+
+
+
+            //}
         }
     }
 }
---
+++ Repositories/GamesSqlRepo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace VideoGameCollection_WinForms.Repositories
+{
+    public class GamesSqlRepo
+    {
+        private const string _connectionString = "server=ORANGE;database=VideoGamesDB;Encrypt=false;UID=palmer;password=palmer";
+        
+        public GamesSqlRepo() { }
+
+        public static void GetGames() //TODO: Return a DataTable, once I've figured out how to populate one
+        {
+            var queryString = $" SELECT * FROM GAMES ";
+            QueryDatabase(queryString, _connectionString);
+        }
+
+        private static void QueryDatabase(string queryString, string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Connection.Open();
+                var data = command.ExecuteReader(); // TODO: Look up how to use ExecuteReader properly and get rows into a DataTable
+                var waitasec = true;
+            }
+        }
+    }
+}
