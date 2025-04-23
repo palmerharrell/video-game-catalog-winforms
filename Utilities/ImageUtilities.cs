68d4e8f012cc0aed63b422fe4ea6a8a1d8863df5
Palmer Harrell <palmerharrell@msn.com>
2025-04-23 08:46:30 -0500

    Adding Utility class for Images

---
+++ Utilities/ImageUtilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoGameCollection_WinForms.Utilities
+{
+    public class ImageUtilities
+    {
+        public static void LoadImageFromDisk()
+        {
+            //TODO: Implement LoadImageFromDisk
+        }
+
+        public static void DisplayImage()
+        {
+            //TODO: Implement DisplayImage
+        }
+    }
+}
