9168ce1bd1a17e08f7c631d8da09edec90acdfe1
Palmer Harrell <palmerharrell@msn.com>
2025-04-29 12:17:31 -0500

    Initial form layout and some binding

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -29,31 +29,234 @@
         private void InitializeComponent()
         {
             btnDebug = new Button();
+            gameList = new ListBox();
+            gameImage = new PictureBox();
+            lblTitle = new Label();
+            lblDescription = new Label();
+            lblGenre = new Label();
+            lblPlatform = new Label();
+            lblReleaseYear = new Label();
+            lblDeveloper = new Label();
+            lblPublisher = new Label();
+            lblPublisherValue = new Label();
+            lblDeveloperValue = new Label();
+            lblReleaseYearValue = new Label();
+            lblPlatformValue = new Label();
+            lblGenreValue = new Label();
+            lblDescriptionValue = new Label();
+            lblTitleValue = new Label();
+            ((System.ComponentModel.ISupportInitialize)gameImage).BeginInit();
             SuspendLayout();
             // 
             // btnDebug
             // 
-            btnDebug.Location = new Point(358, 330);
+            btnDebug.Location = new Point(448, 486);
             btnDebug.Name = "btnDebug";
             btnDebug.Size = new Size(75, 23);
             btnDebug.TabIndex = 0;
             btnDebug.Text = "Debug";
             btnDebug.UseVisualStyleBackColor = true;
-            btnDebug.Click += btnDebug_Click;
+            btnDebug.Click += BtnDebug_Click;
+            // 
+            // gameList
+            // 
+            gameList.FormattingEnabled = true;
+            gameList.ItemHeight = 15;
+            gameList.Location = new Point(10, 10);
+            gameList.Name = "gameList";
+            gameList.Size = new Size(160, 499);
+            gameList.TabIndex = 1;
+            gameList.SelectedIndexChanged += gameList_SelectedIndexChanged;
+            // 
+            // gameImage
+            // 
+            gameImage.Location = new Point(190, 10);
+            gameImage.Name = "gameImage";
+            gameImage.Size = new Size(180, 204);
+            gameImage.TabIndex = 2;
+            gameImage.TabStop = false;
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lblTitle.Location = new Point(392, 12);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(32, 15);
+            lblTitle.TabIndex = 3;
+            lblTitle.Text = "Title";
+            // 
+            // lblDescription
+            // 
+            lblDescription.AutoSize = true;
+            lblDescription.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lblDescription.Location = new Point(392, 174);
+            lblDescription.Name = "lblDescription";
+            lblDescription.Size = new Size(71, 15);
+            lblDescription.TabIndex = 4;
+            lblDescription.Text = "Description";
+            // 
+            // lblGenre
+            // 
+            lblGenre.AutoSize = true;
+            lblGenre.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lblGenre.Location = new Point(392, 39);
+            lblGenre.Name = "lblGenre";
+            lblGenre.Size = new Size(42, 15);
+            lblGenre.TabIndex = 5;
+            lblGenre.Text = "Genre";
+            // 
+            // lblPlatform
+            // 
+            lblPlatform.AutoSize = true;
+            lblPlatform.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lblPlatform.Location = new Point(392, 66);
+            lblPlatform.Name = "lblPlatform";
+            lblPlatform.Size = new Size(56, 15);
+            lblPlatform.TabIndex = 6;
+            lblPlatform.Text = "Platform";
+            // 
+            // lblReleaseYear
+            // 
+            lblReleaseYear.AutoSize = true;
+            lblReleaseYear.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lblReleaseYear.Location = new Point(392, 93);
+            lblReleaseYear.Name = "lblReleaseYear";
+            lblReleaseYear.Size = new Size(77, 15);
+            lblReleaseYear.TabIndex = 8;
+            lblReleaseYear.Text = "Release Year";
+            // 
+            // lblDeveloper
+            // 
+            lblDeveloper.AutoSize = true;
+            lblDeveloper.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lblDeveloper.Location = new Point(392, 120);
+            lblDeveloper.Name = "lblDeveloper";
+            lblDeveloper.Size = new Size(66, 15);
+            lblDeveloper.TabIndex = 9;
+            lblDeveloper.Text = "Developer";
+            // 
+            // lblPublisher
+            // 
+            lblPublisher.AutoSize = true;
+            lblPublisher.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lblPublisher.Location = new Point(392, 147);
+            lblPublisher.Name = "lblPublisher";
+            lblPublisher.Size = new Size(58, 15);
+            lblPublisher.TabIndex = 10;
+            lblPublisher.Text = "Publisher";
+            // 
+            // lblPublisherValue
+            // 
+            lblPublisherValue.AutoSize = true;
+            lblPublisherValue.Location = new Point(493, 147);
+            lblPublisherValue.Name = "lblPublisherValue";
+            lblPublisherValue.Size = new Size(56, 15);
+            lblPublisherValue.TabIndex = 17;
+            lblPublisherValue.Text = "Publisher";
+            // 
+            // lblDeveloperValue
+            // 
+            lblDeveloperValue.AutoSize = true;
+            lblDeveloperValue.Location = new Point(493, 120);
+            lblDeveloperValue.Name = "lblDeveloperValue";
+            lblDeveloperValue.Size = new Size(60, 15);
+            lblDeveloperValue.TabIndex = 16;
+            lblDeveloperValue.Text = "Developer";
+            // 
+            // lblReleaseYearValue
+            // 
+            lblReleaseYearValue.AutoSize = true;
+            lblReleaseYearValue.Location = new Point(493, 93);
+            lblReleaseYearValue.Name = "lblReleaseYearValue";
+            lblReleaseYearValue.Size = new Size(71, 15);
+            lblReleaseYearValue.TabIndex = 15;
+            lblReleaseYearValue.Text = "Release Year";
+            // 
+            // lblPlatformValue
+            // 
+            lblPlatformValue.AutoSize = true;
+            lblPlatformValue.Location = new Point(493, 66);
+            lblPlatformValue.Name = "lblPlatformValue";
+            lblPlatformValue.Size = new Size(53, 15);
+            lblPlatformValue.TabIndex = 14;
+            lblPlatformValue.Text = "Platform";
+            // 
+            // lblGenreValue
+            // 
+            lblGenreValue.AutoSize = true;
+            lblGenreValue.Location = new Point(493, 39);
+            lblGenreValue.Name = "lblGenreValue";
+            lblGenreValue.Size = new Size(38, 15);
+            lblGenreValue.TabIndex = 13;
+            lblGenreValue.Text = "Genre";
+            // 
+            // lblDescriptionValue
+            // 
+            lblDescriptionValue.AutoSize = true;
+            lblDescriptionValue.Location = new Point(493, 174);
+            lblDescriptionValue.Name = "lblDescriptionValue";
+            lblDescriptionValue.Size = new Size(67, 15);
+            lblDescriptionValue.TabIndex = 12;
+            lblDescriptionValue.Text = "Description";
+            // 
+            // lblTitleValue
+            // 
+            lblTitleValue.AutoSize = true;
+            lblTitleValue.Location = new Point(493, 12);
+            lblTitleValue.Name = "lblTitleValue";
+            lblTitleValue.Size = new Size(30, 15);
+            lblTitleValue.TabIndex = 11;
+            lblTitleValue.Text = "Title";
             // 
             // FormMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(917, 518);
+            Controls.Add(lblPublisherValue);
+            Controls.Add(lblDeveloperValue);
+            Controls.Add(lblReleaseYearValue);
+            Controls.Add(lblPlatformValue);
+            Controls.Add(lblGenreValue);
+            Controls.Add(lblDescriptionValue);
+            Controls.Add(lblTitleValue);
+            Controls.Add(lblPublisher);
+            Controls.Add(lblDeveloper);
+            Controls.Add(lblReleaseYear);
+            Controls.Add(lblPlatform);
+            Controls.Add(lblGenre);
+            Controls.Add(lblDescription);
+            Controls.Add(lblTitle);
+            Controls.Add(gameImage);
+            Controls.Add(gameList);
             Controls.Add(btnDebug);
             Name = "FormMain";
             Text = "Video Game Catalog";
+            Load += FormMain_Load;
+            ((System.ComponentModel.ISupportInitialize)gameImage).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button btnDebug;
+        private ListBox gameList;
+        private PictureBox gameImage;
+        private Label lblTitle;
+        private Label lblDescription;
+        private Label lblGenre;
+        private Label lblPlatform;
+        private Label lblReleaseYear;
+        private Label lblDeveloper;
+        private Label lblPublisher;
+        private Label lblPublisherValue;
+        private Label lblDeveloperValue;
+        private Label lblReleaseYearValue;
+        private Label lblPlatformValue;
+        private Label lblGenreValue;
+        private Label lblDescriptionValue;
+        private Label lblTitleValue;
     }
 }
--- FormMain.cs
+++ FormMain.cs
@@ -1,15 +1,95 @@
+using System.Data;
+using VideoGameCollection_WinForms.Repositories;
+
 namespace VideoGameCollection_WinForms
 {
     public partial class FormMain : Form
     {
+        private DataTable games = new();
+        private string loadedGameID = string.Empty;
+
         public FormMain()
         {
             InitializeComponent();
         }
 
-        private void btnDebug_Click(object sender, EventArgs e)
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                ClearBindings();
+                LoadGames();
+                BindGameList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Exception: {ex.Message}", "Error loading games", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void gameList_SelectedIndexChanged(object? sender, EventArgs e)
+        {
+            if (gameList.SelectedItem != null)
+            {
+                DataRowView selectedGame = (DataRowView)gameList.SelectedItem;
+
+                if (selectedGame != null)
+                {
+                    var selectedGameID = selectedGame.Row["VGID"].ToString();
+
+                    if (selectedGameID != null && !string.Equals(selectedGameID, loadedGameID))
+                    {
+                        BindGameDetail(selectedGameID);
+                        loadedGameID = selectedGameID;
+                    }
+                }
+            }
+        }
+
+        private void BtnDebug_Click(object sender, EventArgs e)
         {
             MainDebugger.MainDebug();
         }
+
+        private void LoadGames()
+        {
+            games = GamesSqlRepo.GetGames();
+        }
+
+        private void BindGameList()
+        {
+            gameList.SelectedIndexChanged -= gameList_SelectedIndexChanged;
+            gameList.Items.Clear();
+
+            if (games.Rows.Count > 0)
+            {
+                gameList.DataSource = games;
+                gameList.DisplayMember = "Title";
+                gameList.ClearSelected();
+            }
+
+            gameList.SelectedIndexChanged += gameList_SelectedIndexChanged;
+        }
+
+        private void BindGameDetail(string gameID)
+        {
+            ClearBindings();
+            //TODO: Get game details from games DataTable
+            //TODO: Get game image(s) from database
+            //TODO: Bind details/images to detail controls
+        }
+
+        private void ClearBindings()
+        {
+            lblTitleValue.Text = string.Empty;
+            lblGenreValue.Text = string.Empty;
+            lblPlatformValue.Text = string.Empty;
+            lblReleaseYearValue.Text = string.Empty;
+            lblDeveloperValue.Text = string.Empty;
+            lblPublisherValue.Text = string.Empty;
+            lblDescriptionValue.Text = string.Empty;
+        }
+
     }
 }
